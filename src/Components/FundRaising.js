169029673b56dc59405d4a6eb52aa6869344a571
Rachel <rebmc4@gmail.com>
2019-06-24 21:19:48 +0100

    aligned buttons and images

--- src/Components/FundRaising.js
+++ src/Components/FundRaising.js
@@ -9,38 +9,34 @@ import './Fundraising.css';
 class FundRaising extends Component {
 
     render() {
-        
+
         return (
             <div>
                 <div class="row">
-                    <div class="col-3">
-                            <h4> <img src={paw} alt='logo' class='goldpaw'/>
-                            <button type='button' class='btn btn-outline-dark'> Golden Paw Club </button></h4>
-                            </div>
-                     </div>
-                    <div class="row">
-                    <div class="col-3">
-                            <h4> <img src={dog} alt='logo' class='sponsorDog'/>
-                            <button type='button' class='btn btn-outline-dark'> Sponsor A Sanctuary Dog </button></h4>
-                            </div>
-                     </div>
-                    <div class="row">
-                        <div class="col-3">
-                            <h4> <img src={pawket} alt='logo' class='donate'/>  
-                            <button type='button' class='btn btn-outline-dark'> Pawket Money Toys </button></h4>
-                            </div>
-                     </div>
-                    <div class="row">
-                        <div class="col-3">
-                            <h4> <img src={facebook} alt='logo' class='facebook'/> 
-                            <button type='button' class='btn btn-outline-dark'> Facebook Page </button>
-                            </h4>
-                            </div>
-                     </div>
+                    <div class="col">
+                        <h4> <img src={paw} alt='logo' class='goldpaw' /></h4>
+                        <button type='button' class='btn btn-outline-secondary'> Golden Paw Club </button>
+                    </div>
+
+                    <div class="col">
+                        <h4> <img src={dog} alt='logo' class='sponsorDog' /></h4>
+                        <button type='button' class='btn btn-outline-secondary'> Sponsor A Sanctuary Dog </button>
+                    </div>
+
+                    <div class="col">
+                        <h4> <img src={pawket} alt='logo' class='donate' /></h4>
+                        <button type='button' class='btn btn-outline-secondary'> Pawket Money Toys </button>
+                    </div>
+
+                    <div class="col">
+                        <h4> <img src={facebook} alt='logo' class='facebook' /></h4>
+                        <button type='button' class='btn btn-outline-secondary'> Facebook Page </button>
+                    </div>
+                </div>
             </div>
 
         );
-        
+
     }
 
 }
